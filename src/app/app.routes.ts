9f021ad3e5eadf76c0ea49875842c3122d19098f
Your Name <your_email@example.com>
2024-08-04 14:43:37 +0200

    routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
+import { ContactComponent } from './components/contact/contact.component';
 import { Routes } from '@angular/router';
+import { AboutComponent } from './components/about/about.component';
+import { PortfolioComponent } from './components/portfolio/portfolio.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: "", redirectTo: "about", pathMatch: "full" },
+    { path: "about", component: AboutComponent },
+    { path: "portfolio", component: PortfolioComponent },
+    { path: "contact", component: ContactComponent },
+];
