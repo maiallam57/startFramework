75a296b38dfb60f27f0103b35171e3a4ff2fcb5e
Your Name <your_email@example.com>
2024-08-06 16:17:02 +0200

    navbarTransition

--- startFramework/src/app/components/navbar/navbar.component.ts
+++ startFramework/src/app/components/navbar/navbar.component.ts
@@ -1,13 +1,20 @@
-import { Component } from '@angular/core';
+import { NgClass } from '@angular/common';
+import { Component, HostListener } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [RouterLink, RouterLinkActive],
+  imports: [RouterLink, RouterLinkActive, NgClass],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
+  isScrolled: boolean = false;
+
+  @HostListener('window:scroll', [])
+  onWindowScroll(): void {
+    this.isScrolled = window.pageYOffset > 50;  // Adjust this value as needed
+  }
 
 }
